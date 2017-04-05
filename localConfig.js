4a4acebc0c60926dd26f628a4aba9a12846324d8
Itziar Sesma <itziar.sesma.u@gmail.com>
2017-04-06 00:05:58 +0200

    Se añade el POST de autenticacion para la generación de tokens

--- localConfig.js
+++ localConfig.js
@@ -4,5 +4,9 @@ module.exports = {
     DB_NAME: 'nodepop',
     INITIAL_DATA: 'initialData.json',
     ERROR_MESSAGES: 'errorMessages.json',
-    DEFAULT_ERROR_STATUS: 500
+    DEFAULT_ERROR_STATUS: 500,
+    jwt: {
+        EXPIRES_IN: "2h",
+        SECRET_WORD: "nodepop_segura"
+    }
 }
--- routes/apiv1/users.js
+++ routes/apiv1/users.js
@@ -4,8 +4,10 @@ const express = require('express');
 const router = express.Router();
 
 const sha256 = require('sha256');
+const jwt = require('jsonwebtoken');
 
 const customError = require('../../lib/customError');
+const config = require('../../localConfig');
 
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
@@ -31,9 +33,32 @@ router.post("/", function(req, res, next) {
                 return customError(req, res, 'USR_ADD_ERROR');
             }
         }
-        res.json({sucess: true, adverts: user});
+        res.json({sucess: true, user: user});
     });
 
+});
+
+router.post("/authenticate", function(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}).exec(function(err, user) {
+        if (err) {
+            return next(err);
+        }
+        if(!user) {
+            return customError(req, res, 'USR_NOT_FOUND', 401);
+        }
+        if(!password || user.password !== sha256(password)) {
+            return customError(req, res, 'USR_BAD_PWD', 401);
+        }
+        jwt.sign({email: email}, config.jwt.SECRET_WORD, {expiresIn: config.jwt.EXPIRES_IN}, function(err, token) {
+            if(err) {
+                return customError(req, res, 'USR_SIGN_ERR');
+            }
+            res.json({success: true, token: token});
+        });
+    });
 });
 
 module.exports = router;
